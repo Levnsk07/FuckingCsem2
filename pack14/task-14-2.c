c557c7c33159453c6f190c928be078ecc8e7d96b
Levnsk07 <Levnsk07@gmail.com>
2026-03-03 17:09:34 +0700

    fix from pack14 tasks: 2, 5 remake without global variables (only pointers).

--- pack14/task-14-2.c
+++ pack14/task-14-2.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #define MAX_N 2000
 
-long long fact[MAX_N + 1];
-long long inv_fact[MAX_N + 1];
-long long M;
-
 long long int pow_mod(long long int a, long long int b, long long int mod) {    // см task-14-1
     long long int result = 1;
     a %= mod;
@@ -17,19 +13,18 @@ long long int pow_mod(long long int a, long long int b, long long int mod) {    
     }
     return result;
 }
-void precompute_factorials(long long m) {   //  создание двух массивов факториалов (обычного и обратного)
-    M = m;
+void precompute_factorials(long long m,long long *fact,long long *inv_fact) {   //  создание двух массивов факториалов (обычного и обратного)
     fact[0] = 1;
     fact[1] = 1;
     for (int i = 2; i <= MAX_N; ++i) {
-        fact[i] = (fact[i - 1] * i) % M;
+        fact[i] = (fact[i - 1] * i) % m;
     }
 
-    inv_fact[0] =pow_mod(fact[0], M - 2, M);
-    inv_fact[1] =pow_mod(fact[1], M - 2, M);
+    inv_fact[0] =pow_mod(fact[0], m - 2, m);
+    inv_fact[1] =pow_mod(fact[1], m - 2, m);
 
     for (int i = 2; i <= MAX_N; ++i) {
-        inv_fact[i] = pow_mod((fact[i]) ,m-2, M);
+        inv_fact[i] = pow_mod((fact[i]) ,m-2, m);
     }
 
 }
@@ -38,9 +33,12 @@ void precompute_factorials(long long m) {   //  создание двух мас
 int main() {
     freopen("input.txt", "r", stdin);
 
+    long long fact[MAX_N + 1];
+    long long inv_fact[MAX_N + 1];
+
     long m, t;
     scanf("%ld %ld", &m, &t);
-    precompute_factorials(m);
+    precompute_factorials(m,fact,inv_fact);
 
     for (int i = 0; i < t; ++i) {
         int n, k;
@@ -48,7 +46,7 @@ int main() {
         if (n < k || k < 0) {
             printf("0\n");
         } else {
-            printf("%lld\n", (fact[n]*  inv_fact[k] * inv_fact[n - k]) % M);    //  в свойствах модулей нет деления, но есть умножение, поэтому вместу деление обратное
+            printf("%lld\n", (fact[n]*  inv_fact[k] * inv_fact[n - k]) % m);
         }
     }
 
--- pack14/task-14-5.c
+++ pack14/task-14-5.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 
-#define MAX_N 1000000
-
-long long fact[MAX_N + 1];
-long long inv_fact[MAX_N + 1];
-long long M;
+#define MAX_N 100000
 
 long long int pow_mod(long long int a, long long int b, long long int mod) {
     // Теорема Фермы
@@ -20,60 +16,45 @@ long long int pow_mod(long long int a, long long int b, long long int mod) {
     return result;
 }
 
-void precompute_factorials(long long m) {
-    // Совойство модудей
-    M = m;
+void precompute_factorials(long long m, long long *fact, long long *inv_fact) {
+    m = m;
     fact[0] = 1;
     fact[1] = 1;
     for (int i = 2; i <= MAX_N; ++i) {
-        fact[i] = (fact[i - 1] * i) % M;
+        fact[i] = (fact[i - 1] * i) % m;
     }
 
-    inv_fact[0] = pow_mod(fact[0], M - 2, M);
-    inv_fact[1] = pow_mod(fact[1], M - 2, M);
+    inv_fact[0] = pow_mod(fact[0], m - 2, m);
+    inv_fact[1] = pow_mod(fact[1], m - 2, m);
     for (int i = 2; i <= MAX_N; ++i) {
-        inv_fact[i] = pow_mod((fact[i]), m - 2, M);
+        inv_fact[i] = pow_mod((fact[i]), m - 2, m);
     }
 }
 
 
 int main() {
+
+    long long fact[MAX_N + 1];
+    long long inv_fact[MAX_N + 1];
+
     freopen("input.txt", "r", stdin);
 
     unsigned int m = 1000000007;
 
     long t;
     scanf("%ld", &t);
-    precompute_factorials(m);
-
-    // for (int i = 0; i <= 120; ++i) {
-    //     printf("%lld ", fact[i]);
-    // }
-    // printf("\n");
-    //
-    // for (int i = 0; i <= 60; ++i) {
-    //     printf("%lld ", inv_fact[i]);
-    // }
-    // printf("\n");
-
+    precompute_factorials(m,fact,inv_fact);
 
     for (int i = 0; i < t; ++i) {
         int n, k;
         scanf("%d %d", &n, &k);
-        // printf("%lld %lld %d\n", n, k, n - k);
         if (n < k || k < 0) {
             printf("0\n");
         } else {
-            // printf("%lld %lld %lld %llu\n", fact[n], inv_fact[k], inv_fact[n - k],(fact[n] * inv_fact[k] * inv_fact[n - k]));
-            // printf("%llu\n", ( (unsigned long long int) fact[n] * inv_fact[k] * inv_fact[n - k] ) % M);
-            printf("%llu\n", ( (unsigned long long int) fact[n] % M * inv_fact[k]% M * inv_fact[n - k]% M ) % M);
+            printf("%llu\n", ((unsigned long long int) fact[n] % m * inv_fact[k] % m * inv_fact[n - k] % m) % m);
         }
     }
 
     fclose(stdin);
-
-
     return 0;
 }
-
-
